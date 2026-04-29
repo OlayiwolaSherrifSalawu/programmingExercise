96e09dacf48b716796e0f43df5b63f52d1c13f69
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-29 15:38:53 +0100

    stripHTML

--- slidingWindow/ans.go
+++ slidingWindow/ans.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println(maxTemp([]int{70, 75, 80, 72, 74, 90, 85, 88, 86, 80, 60}, 3))
-	fmt.Println(correctOrder("act", "cat"))
+	// fmt.Println(maxTemp([]int{70, 75, 80, 72, 74, 90, 85, 88, 86, 80, 60}, 3))
+	// fmt.Println(correctOrder("act", "cat"))
+	fmt.Println(stripHtml("<b>bold</b> <html>ola</html>"))
 }
 
 func maxTemp(slic []int, n int) ([]int, int) {
@@ -43,4 +46,35 @@ func correctOrder(word, target string) bool {
 		return true
 	}
 	return false
+}
+func stripHtml(s string) string {
+	tag := map[string]string{
+		"<":  ">",
+		"</": ">",
+	}
+	store := ""
+	tags := ""
+	foundTag := false
+	for i := 0; i < len(s); i++ {
+		val, ok := tag[string(s[i])]
+		if ok {
+			tags = val
+
+		}
+		if ok {
+			foundTag = true
+			continue
+		}
+		if string(s[i]) == tags {
+			foundTag = false
+			continue
+		}
+		if !ok && !foundTag {
+			store += string(s[i])
+		}
+		if !ok && foundTag {
+			continue
+		}
+	}
+	return store
 }
