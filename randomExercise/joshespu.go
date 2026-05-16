8dae2866ee9bfc92de1d7e8bf9cdb2f8e11d1d41
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-16 17:40:15 +0100

    updated josepu function

--- randomExercise/joshespu.go
+++ randomExercise/joshespu.go
@@ -9,25 +9,19 @@ func main() {
 	fmt.Println(JospehPermutation([]int{1, 2, 3, 4, 5, 6, 7}, 3))
 }
 func JospehPermutation(s []int, kill int) []int {
-
 	res := []int{}
 	killCount := 1
 	for len(s) > 0 {
-		if kill > len(s) {
-			if killCount == kill {
-				res = append(res, (len(s)%killCount)-1)
-				s = slices.Delete(s, (len(s)%killCount)-1, (len(s) % killCount))
-				killCount = 1
-			}
-		}
-		if killCount == kill {
-			res = append(res, s[killCount-1])
-			s = slices.Delete(s, killCount-1, killCount)
+		targetedPerson := s[0]
+		if kill == killCount {
+			res = append(res, s[0])
+			s = slices.Delete(s, 0, 1)
+
 			killCount = 1
+		} else {
+			s = slices.Delete(s, 0, 1)
+			s = append(s, targetedPerson)
 		}
-		theVal := s[killCount-1]
-		s = slices.Delete(s, killCount-1, killCount)
-		s = append(s, theVal)
 		killCount++
 	}
 	return res
