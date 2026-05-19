c2a8b44b15e242ca3962f101ced4cc394220c33a
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-05-19 14:57:48 +0100

    working on range

--- randomExercise/cdw5.go
+++ randomExercise/cdw5.go
@@ -7,40 +7,29 @@ import (
 
 func ranges(s []int) string {
 	var result strings.Builder
-
 	start := 0
 	end := 0
-
 	count := 0
 	hasChanged := false
+	toChangeSt := false
+	sLen := len(s)
 	for i, val := range s {
-		checkIf := val + 1
-		if i+1 < len(s) && s[i+1] == checkIf {
-			if !hasChanged {
-				start = i
-				hasChanged = true
-			}
-			count++
-
-		}
-		if count == 2 && hasChanged {
-			end = i + 1
-			result.WriteString(strconv.Itoa(s[start]))
-			result.WriteString("-")
-			result.WriteString(strconv.Itoa(s[end]))
-			result.WriteString(",")
+		if hasChanged {
 			hasChanged = false
 			continue
 		}
-		if i+1 < len(s) && s[i+1] != checkIf && count > 0 {
-			end = i + 1
-			result.WriteString(strconv.Itoa(s[end]))
+		checkIf := val + 1
+		thENd := i + 1
+		if count < 2 && thENd < sLen && s[thENd] != checkIf {
+			result.WriteString(strconv.Itoa(s[thENd]))
 			result.WriteString(",")
-			count = 0
-
 			continue
 		}
-
+		if thENd < sLen && s[thENd] == checkIf {
+			if !toChangeSt {
+				
+			}
+		}
 	}
 	return result.String()
 }
