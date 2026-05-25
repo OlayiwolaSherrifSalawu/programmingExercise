f9f1501b9ff5f55cdb45afe41c4bc4298f024ac8
Olayiwola Sherrif Salawu <salawuolayiwola181@gmail.com>
2026-05-25 23:26:39 +0100

    upgraded to fix edge case

--- randomExercise/main.go
+++ randomExercise/main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(removeZeros([]int{2, 3, 0, 4, 0, 9, 0}))
+	fmt.Println([]int{2, 3, 0, 4, 0, 0, 0, 0, 0, 0, 9, 0})
+	fmt.Println(removeZeros([]int{2, 3, 0, 4, 0, 0, 0, 0, 0, 0, 9, 0}))
 }
--- randomExercise/removeZero.go
+++ randomExercise/removeZero.go
@@ -3,11 +3,19 @@ package main
 import "slices"
 
 func removeZeros(si []int) []int {
-	for i := 0; i < len(si); i++ {
+	count := 0
+	i := 0
+	for {
 		theO := si[i]
 		if si[i] == 0 {
 			si = slices.Delete(si, i, i+1)
 			si = append(si, theO)
+			count++
+			i--
+		}
+		i++
+		if count+i == len(si)-1 {
+			break
 		}
 	}
 	return si
