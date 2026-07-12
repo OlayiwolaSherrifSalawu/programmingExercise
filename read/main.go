2645d6a523677c63b0b0bc77865ed4724f0d9445
Olayiwola Sherrif Salawu <108702346+OlayiwolaSherrifSalawu@users.noreply.github.com>
2026-07-12 22:37:05 +0100

    rpn cal solved baby

--- read/main.go
+++ read/main.go
@@ -1,32 +1,58 @@
-package main
-
-import (
-	"strconv"
-	"strings"
-)
-
 func main() {
 	// longest sub string in a string
-
+	fmt.Println(rpnCalc("     1      3 * ksd 2 -"))
 }
-func rpnCalc(s string) string{
-	operator:= "-+/%*"
+func rpnCalc(s string) string {
+	operator := "-+/%*"
 	rpn := strings.Fields(s)
 	stack := []int{}
-	for i:= 0; i < len(rpn); i++{
-		op:= ""
-		nu, err:= strconv.Atoi(rpn[i])
-		if err != nil{
-			if strings.ContainsAny(operator, rpn[i]){
-				op= rpn[i]
-			}else{
+	for i := 0; i < len(rpn); i++ {
+		op := ""
+		nu, err := strconv.Atoi(rpn[i])
+		if err != nil {
+			if strings.ContainsAny(operator, rpn[i]) && stack != nil {
+				op = rpn[i]
+				err := opSolve(op, &stack)
+				if err != nil {
+					return err.Error()
+				}
+			} else {
 				return "ERROR"
 			}
+		} else {
+			stack = append(stack, nu)
 		}
+		fmt.Println(stack)
 	}
-
+	if len(stack) != 1 {
+		return "ERROR"
+	}
+	result := strconv.Itoa(stack[0])
+	return result
 }
 
-func opSolve(s string, st []int) string {
-	
+func opSolve(op string, st *[]int) (err error) {
+	var num int
+	defer func() {
+		if re := recover(); re != nil {
+			err = fmt.Errorf("ERROR")
+		}
+	}() // this is to catch index out of range panic error
+
+	last, bLast := (*st)[len((*st))-1], (*st)[len((*st))-2]
+	*st = slices.Delete(*st, len(*st)-2, len(*st))
+	switch op {
+	case "+":
+		num = bLast + last
+	case "*":
+		num = bLast * last
+	case "-":
+		num = bLast - last
+	case "/":
+		num = bLast / last
+	case "%":
+		num = bLast % last
+	}
+	*st = append(*st, num)
+	return nil
 }
